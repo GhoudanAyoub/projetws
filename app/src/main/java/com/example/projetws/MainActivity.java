85e32a0727f524a4dc246a358e2ef4cbc4c22218
ayoub <ayoubghoudanos@gmail.com>
2022-03-15 19:29:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projetws/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.projetws;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText nom;
+    private EditText prenom;
+    private Spinner ville;
+    private RadioButton m;
+    private RadioButton f;
+    private Button add;
+    RequestQueue requestQueue;
+    String insertUrl = "http://192.168.43.228/php02_1/ws/createEtudiant.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nom = (EditText) findViewById(R.id.nom);
+        prenom = (EditText) findViewById(R.id.prenom);
+        ville = (Spinner) findViewById(R.id.ville);
+        add = (Button) findViewById(R.id.add);
+        m = (RadioButton) findViewById(R.id.m);
+        f = (RadioButton) findViewById(R.id.f);
+        add.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("ok", "ok");
+        if (v == add) {
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+            StringRequest request = new StringRequest(Request.Method.POST,
+                    insertUrl, response -> {
+                Log.d("TAG", response);
+                Type type = new TypeToken<Collection<Etudiant>>(){}.getType();
+                Collection<Etudiant> etudiants = new Gson().fromJson(response, type);
+                for(Etudiant e : etudiants){
+                    Log.d("TAG", e.toString());
+                }
+            }, error -> {
+
+            }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    String sexe = "";
+                    if (m.isChecked())
+                        sexe = "homme";
+                    else
+                        sexe = "femme";
+                    HashMap<String, String> params = new HashMap<>();
+                    params.put("nom", nom.getText().toString());
+                    params.put("prenom", prenom.getText().toString());
+                    params.put("ville", ville.getSelectedItem().toString());
+                    params.put("sexe", sexe);
+                    return params;
+                }
+            };
+            requestQueue.add(request);
+        }
+    }
+}
