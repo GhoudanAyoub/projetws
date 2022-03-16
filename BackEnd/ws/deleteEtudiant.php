96c497bb1af529d684941980f853ffee86c2bfbb
ayoub <ayoubghoudanos@gmail.com>
2022-03-16 21:43:37 +0100

    backEnd

--- BackEnd/ws/deleteEtudiant.php
+++ BackEnd/ws/deleteEtudiant.php
@@ -1,12 +1,14 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     chdir("..");
-    include_once  __DIR__ . './../service/EtudiantService.php';
-    loadAll();
+    include_once __DIR__ . './../service/EtudiantService.php';
+    delete();
 }
-function loadAll()
+function delete()
 {
+    extract($_POST);
     $es = new EtudiantService();
+    $es->delete($es->findById(intval($id)));
     header('Content-type: application/json');
     echo json_encode($es->findAllApi());
 }
