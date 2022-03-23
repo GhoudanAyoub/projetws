97f616f549632113d20e7d17bf9a48d6ade680be
ayoub <ayoubghoudanos@gmail.com>
2022-03-23 23:15:24 +0100

    Clean backEnd

--- BackEnd/ws/createEtudiant.php
+++ BackEnd/ws/createEtudiant.php
@@ -9,19 +9,14 @@ function create()
     extract($_POST);
     $es = new EtudiantService();
 
-    $image = base64_to_jpeg($img, 'uploaded.jpg');
-
-
+    if($img == "no") {
+        $image = null;
+    }else {
+        $image = $img;
+    }
+    
     $es->create(new Etudiant(1, $nom, $prenom, $ville, $sexe, $image));
     header('Content-type: application/json');
     echo json_encode($es->findAllApi());
 }
 
-
-function base64_to_jpeg($base64_string, $output_file)
-{
-    $ifp = fopen($output_file, "wb");
-    fwrite($ifp, base64_decode($base64_string));
-    fclose($ifp);
-    return $output_file;
-}
